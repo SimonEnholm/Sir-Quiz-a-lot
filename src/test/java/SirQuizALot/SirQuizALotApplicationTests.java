7ea165cc295ce684b1891d864b7354dd3790a453
Joel Rosenstam <rosenstam.joel@gmail.com>
2022-02-23 15:00:57 +0100

    Initial

---
+++ src/test/java/SirQuizALot/SirQuizALotApplicationTests.java
@@ -0,0 +1,13 @@
+package SirQuizALot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SirQuizALotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
