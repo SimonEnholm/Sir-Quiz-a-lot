1b4a17ad9ceb79d7caada091277d285adfc2a796
Joel Rosenstam <rosenstam.joel@gmail.com>
2022-02-24 15:34:27 +0100

    lade till ytterligare en användare för test

--- src/main/java/SirQuizALot/UserRepo.java
+++ src/main/java/SirQuizALot/UserRepo.java
@@ -16,6 +16,7 @@ public class UserRepo {
 
         userList.add(admin);
         userList.add(user);
+        userList.add(new User(3,"GöranPersson", "allaskamed", false));
     }
 
     public List<User> getUserList() {
