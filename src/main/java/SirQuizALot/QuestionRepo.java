2f364d59c1ec28c169250ceb90f4b673932391d0
Joel Rosenstam <rosenstam.joel@gmail.com>
2022-02-24 11:56:05 +0100

    skapade getquestion

--- src/main/java/SirQuizALot/QuestionRepo.java
+++ src/main/java/SirQuizALot/QuestionRepo.java
@@ -1,23 +1,43 @@
 package SirQuizALot;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
-@Repository
+@Service
 public class QuestionRepo {
 
-    void createQuestion() {
-        Questions q1 = new Questions(1,"Vilket land konsumerar mest Coca Cola per invånare?",3); // Island
-        Questions q2 = new Questions(2,"Hur många rutor finns det på ett vanligt rutigt A4 papper?",3); // 1029
-        Questions q3 = new Questions(3,"Hur länge varade världshistoriens kortaste krig mellan Storbritannien och Zanzibar?",3); // 38 min
+    List<Questions> qList = new ArrayList<>();
 
-        List<Questions> qList = new ArrayList<>();
+    public QuestionRepo() {
+        Questions q1 = new Questions(1,
+                "Vilket land konsumerar mest Coca Cola per invånare?",
+                "Sverige",
+                "U.S.A.",
+                "Island",
+                3);
+        Questions q2 = new Questions(2,
+                "Hur många rutor finns det på ett vanligt rutigt A4 papper?",
+                "1027",
+                "1028",
+                "1029",
+                3); // 1029
+        Questions q3 = new Questions(3,
+                "Hur länge varade världshistoriens kortaste krig mellan Storbritannien och Zanzibar?",
+                "7 min",
+                "2,5 timme",
+                "38 min",
+                3); // 38 min
+
         qList.add(q1);
         qList.add(q2);
         qList.add(q3);
-
     }
 
+    public Questions getQuestion() {
+        return qList.get(ThreadLocalRandom.current().nextInt(0, qList.size()));
+    }
 }
