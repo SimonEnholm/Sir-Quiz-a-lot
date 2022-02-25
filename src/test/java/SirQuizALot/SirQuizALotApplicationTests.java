83cba0fd6194479abb803e7a05ff5a05ddbee7a7
Joel Rosenstam <rosenstam.joel@gmail.com>
2022-02-25 14:59:20 +0100

    Lade till 2 tester

--- src/test/java/SirQuizALot/SirQuizALotApplicationTests.java
+++ src/test/java/SirQuizALot/SirQuizALotApplicationTests.java
@@ -24,6 +24,27 @@ class SirQuizALotApplicationTests {
 		Assertions.assertEquals(false, service.isUser("göranPERSSON","ALLAskaMED"));
 	}
 
+	@Test
+	void testCreateAQuestion() {
+		SirQuizALotService service = new SirQuizALotService();
+		List<Questions> questionsList = service.getAllQuestions();
+		int lengthList = questionsList.size();
+		service.createQuestion(101,"Fråga","alt1","alt2","alt3",1);
+		Questions questions = new Questions(101,"Fråga","alt1","alt2","alt3",1);
+		questionsList = service.getAllQuestions();
+		Assertions.assertEquals(true, lengthList+1 == questionsList.size());
+		Assertions.assertEquals(true, questions.getId() == questionsList.get(questionsList.size()-1).getId());
+	}
+
+	@Test
+	void testGetQuizList() {
+		SirQuizALotService service = new SirQuizALotService();
+		List<Questions> questionsList = service.getListOfQuestions();
+		List<Questions> questionsList2 = service.getListOfQuestions();
+
+		Assertions.assertEquals(5, questionsList.size());
+		Assertions.assertNotEquals(questionsList, questionsList2);
+	}
 
 
 }
